38ef6dc669939b8fc5987851a707727bf0c9b788
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-02 21:08:19 +0100

    la lista degli insegnanti è ora dinamica, l'esercizio viene svolto semplicemente cliccando sui checkmark

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -7,46 +7,80 @@ const teachers = [
   'Lewis',
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
-
-// crea lista insegnanti a caricamento pagina
-for (let i = 0; i < teachers.length; i++) {
-  let currentTeacher = teachers[i];
-  document.getElementById("teachers").innerHTML += `<div class="col text-center"><p> ${currentTeacher} </p><img style="height: 50px;" src="../img/stick-man.png"></div>`;
-  
+document.getElementById("consoleOutput")
+// rappresenta a schermo l'array degli insegnanti
+function drawTeachers() {
+  document.getElementById("teachers").innerHTML = "";
+  for (let i = 0; i < teachers.length; i++) {
+    let currentTeacher = teachers[i];
+    document.getElementById("teachers").innerHTML += `<div class="col text-center"><p> ${currentTeacher} </p><img style="height: 50px;" src="../img/stick-man.png"></div>`;
+  }
 }
+
+drawTeachers();
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers[3];
-console.log(fourthTeacher);
+function arrays011() {
+  const fourthTeacher = teachers[3];
+  document.getElementById("consoleOutput").value = `fourthTeacher = ${fourthTeacher}`;
+  document.getElementById("arrays011").disabled = true;
+}
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers[4] = "Patrick";
-console.log(teachers[4]);
+function arrays012() {
+  teachers[4] = "Patrick";
+  document.getElementById("consoleOutput").value = `teachers[4] = ${teachers[4]}`;
+  document.getElementById("arrays012").disabled = true;
+  drawTeachers();
+}
 
-// 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers.pop();
-console.log(lastTeacher);
+// // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
+function arrays013() {
+  const lastTeacher = teachers.pop();
+  document.getElementById("consoleOutput").value = `lastTeacher = ${lastTeacher}`;
+  document.getElementById("arrays013").disabled = true;
+  drawTeachers();
+}
 
-// 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = teachers.shift();
-console.log(firstTeacher);
+// // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+function arrays014() {
+  const firstTeacher = teachers.shift();
+  document.getElementById("consoleOutput").value = `firstTeacher = ${firstTeacher}`;
+  document.getElementById("arrays014").disabled = true;
+  drawTeachers();
+}
 
-// 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.push("Vanessa")
-console.log(teachers);
+// // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+function arrays015() {
+  teachers.push("Vanessa")
+  document.getElementById("consoleOutput").value = `teachers[latest_entry] = ${teachers[teachers.lenght]}`;
+  document.getElementById("arrays015").disabled = true;
+  drawTeachers();
+}
 
-// 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-teachers.unshift("Sarah");
-console.log(teachers[0]);
+// // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+function arrays016() {
+  teachers.unshift("Sarah");
+  document.getElementById("consoleOutput").value = `firstTeacher = ${teachers[0]}`;
+  document.getElementById("arrays016").disabled = true;
+  drawTeachers();
+}
 
-// 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
-// e salvalo nella variabile lewisIndex
-const lewisIndex = teachers.indexOf("Lewis");
-console.log(lewisIndex);
+// // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
+// // e salvalo nella variabile lewisIndex
+function arrays017() {
+  const lewisIndex = teachers.indexOf("Lewis");
+  document.getElementById("consoleOutput").value = `lewisIndex = ${lewisIndex}`;
+  document.getElementById("arrays017").disabled = true;
+}
 
 
-// 8. Verifica se l'array teachers è vuoto
-// e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = Boolean(teachers.lenght);
-console.log(isTeachersEmpty);
+// // 8. Verifica se l'array teachers è vuoto
+// // e salva il risultato nella variabile isTeachersEmpty
+function arrays018() {
+  const isTeachersEmpty = Boolean(teachers.lenght);
+  document.getElementById("consoleOutput").value = `isTeachersEmpty = ${isTeachersEmpty}`;
+  document.getElementById("arrays018").disabled = true;
+}
+
